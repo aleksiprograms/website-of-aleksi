73b95763b95893d26bde8d5a5f365d6a532bdbbd
Aleksi Tolvanen <aleksiprograms@gmail.com>
2020-07-31 15:37:24 +0300

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+    return (
+        <div>
+            <h1 className="title">Website of Aleksi</h1>
+        </div>
+    );
+}
+
+export default App;
