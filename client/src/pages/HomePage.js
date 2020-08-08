097d67d09293043c21072ef603ec6454f7d82558
Aleksi Tolvanen <aleksiprograms@gmail.com>
2020-08-08 21:38:53 +0300

    Create homepage

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const HomePage = () => {
     return (
-        <div>
-            <h1>Home</h1>
+        <div className="homeContainer">
+            <div className="homeTextContainer">
+                <h1 className="homeTitle">Hey, I am Aleksi</h1>
+            </div>
+            <div className="homeTextContainer">
+                <p className="homeText">
+                    I am mainly self-taught programmer/developer and
+                    I am interested in mobiledev, webdev and gamedev.
+                </p>
+            </div>
+            <Link
+                className="button"
+                to="/projects"
+            >PROJECTS</Link>
+            <img
+                className="imageHome"
+                src="https://dl.dropboxusercontent.com/s/q2mf0ugw4ohzrlm/battleagainstshapesart.png?dl=0"
+            />
         </div>
     );
 };
