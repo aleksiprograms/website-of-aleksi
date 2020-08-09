67f2cfda67da2cc3179e4213d355adb5d5f9c844
Aleksi Tolvanen <aleksiprograms@gmail.com>
2020-08-09 18:35:55 +0300

    Prepare for release

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 
 const projects = require('./routes/api/projects');
+const { request, response } = require('express');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -10,6 +12,13 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/projects', projects);
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+    app.get('*', (request, response) => {
+        response.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
