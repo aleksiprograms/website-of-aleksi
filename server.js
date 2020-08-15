89a1a629d18678b71b042f8fb53953b54db3a209
Aleksi Tolvanen <aleksiprograms@gmail.com>
2020-08-15 20:13:22 +0300

    Move https redirect before any routes

--- server.js
+++ server.js
@@ -13,10 +13,6 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/projects', projects);
-app.use('/admin', admin);
-app.use('/login', login);
-
 if (process.env.NODE_ENV === 'production') {
     app.use((request, response, next) => {
         if (request.header('x-forwarded-proto') !== 'https') {
@@ -26,6 +22,10 @@ if (process.env.NODE_ENV === 'production') {
         }
     });
 }
+
+app.use('/api/projects', projects);
+app.use('/admin', admin);
+app.use('/login', login);
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
