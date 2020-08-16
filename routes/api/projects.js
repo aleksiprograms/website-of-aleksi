6affe98027841c168e5a1ac85f7456f72e9f67c4
Aleksi Tolvanen <aleksiprograms@gmail.com>
2020-08-16 15:21:03 +0300

    Add admin functionality to backend

--- routes/api/projects.js
+++ routes/api/projects.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const jwt = require('jsonwebtoken');
 const { Client } = require('pg');
 
 // Table of database creation
@@ -16,58 +17,6 @@ CREATE TABLE projects (
 );
 */
 
-projectsOld = [
-    {
-        id: 1,
-        title: "Website of Aleksi",
-        text: "The site you are in right now is this project. This is my personal website to show my projects. At the moment all the data is saved in the backend code. The plan in the future is to use a PostgreSQL database and make an admin page, where I can modify projects.",
-        platforms: "Web",
-        technologies: "React, Node.js, Express, JavaScript",
-        githubUrl: "https://github.com/aleksiprograms/website-of-aleksi",
-        imageOrientation: "none"
-    },
-    {
-        id: 2,
-        title: "Battle Against Shapes",
-        text: "Battle Against Shapes is a 2D game, where you fight with the armed ship against different shaped enemies. Your goal is to get as far as you can in your choosen difficulty. As the difficulty increases the number of different enemies increases. There are eight different kind of enemies and they have their own type of attacking method for example they shoot, explode near you or just attack you. You can choose one primary and one secondary weapon from eight different kind of weapons.",
-        platforms: "Android, Desktop",
-        technologies: "libGDX, Java",
-        githubUrl: "https://github.com/aleksiprograms/battle-against-shapes",
-        imageUrl: "https://dl.dropboxusercontent.com/s/b5q8qe37sxxb3nj/battleagainstshapesgameplay.png?dl=0",
-        imageOrientation: "landscape"
-    },
-    {
-        id: 3,
-        title: "Survival of Keijo",
-        text: "Survival of Keijo is an unfinished 3D game, where you try to survive against waves of armed enemies. There is a shop, where you can buy weapons and ammunition, and upgrade your weapons.",
-        platforms: "Android, Desktop",
-        technologies: "libGDX, Java",
-        githubUrl: "https://github.com/aleksiprograms/survival-of-keijo",
-        imageUrl: "https://dl.dropboxusercontent.com/s/1stl04kf3gdkvlr/survivalofkeijogameplay.jpg?dl=0",
-        imageOrientation: "landscape"
-    },
-    {
-        id: 4,
-        title: "Stopwatch",
-        text: "This is a stopwatch app, where you can make multible stopwatches and run them at the same time. The app is not meant for very accurate measurements (accuracy is about plus-minus 0.1 s).",
-        platforms: "Android, iOS",
-        technologies: "React Native, JavaScript",
-        githubUrl: "https://github.com/aleksiprograms/stopwatch",
-        imageUrl: "https://dl.dropboxusercontent.com/s/vaa11eef3nz84g6/stopwatchmain.jpg?dl=0",
-        imageOrientation: "portrait"
-    },
-    {
-        id: 5,
-        title: "Calculator",
-        text: "This is a calculator app, where you can make quite complex calculations. The calculation are saved to history and you can reuse them or make variable from its result. You can make your own variables/constants and use them in your calculations.",
-        platforms: "Android",
-        technologies: "Android, Java",
-        githubUrl: "https://github.com/aleksiprograms/calculator",
-        imageUrl: "https://dl.dropboxusercontent.com/s/3favd2pwi4d3hjn/calculatormain.jpg?dl=0",
-        imageOrientation: "portrait"
-    },
-];
-
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config();
 }
@@ -79,6 +28,23 @@ const client = new Client({
     }
 });
 client.connect();
+
+const isAuthorized = (request) => {
+    const token = getTokenFrom(request);
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+    if (!token || !decodedToken.username) {
+        return false;
+    }
+    return true;
+}
+
+const getTokenFrom = (request) => {
+    const authorization = request.get('authorization');
+    if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+        return authorization.substring(7);
+    }
+    return null;
+}
 
 router.get('/', async (request, response) => {
     let res = await client.query('SELECT * FROM projects ORDER BY placeinprojects ASC');
@@ -95,7 +61,78 @@ router.get('/', async (request, response) => {
             imageOrientation: row.imageorientation
         });
     }
-    response.json(projects);
+    response.status(200).json(projects);
+});
+
+router.post('/', async (request, response) => {
+    if (!isAuthorized(request)) {
+        return response.status(401).json({ error: 'token missing or invalid' });
+    }
+    const body = request.body;
+    await client.query(
+        'INSERT INTO projects '
+        + '('
+        + 'title, '
+        + 'text, '
+        + 'platforms, '
+        + 'technologies, '
+        + 'githuburl, '
+        + 'imageurl, '
+        + 'imageorientation, '
+        + 'placeinprojects'
+        + ') '
+        + 'VALUES($1, $2, $3, $4, $5, $6, $7, $8)',
+        [
+            body.title,
+            body.text,
+            body.platforms,
+            body.technologies,
+            body.githubUrl,
+            body.imageUrl,
+            body.imageOrientation,
+            body.placeInProjects]
+    );
+    response.sendStatus(200);
+});
+
+router.put('/:id', async (request, response) => {
+    if (!isAuthorized(request)) {
+        return response.status(401).json({ error: 'token missing or invalid' });
+    }
+    const body = request.body;
+    await client.query(
+        'UPDATE projects '
+        + 'SET '
+        + 'title = $1, '
+        + 'text = $2, '
+        + 'platforms = $3, '
+        + 'technologies = $4, '
+        + 'githuburl = $5, '
+        + 'imageurl = $6, '
+        + 'imageorientation = $7, '
+        + 'placeinprojects = $8 '
+        + 'WHERE id = $9',
+        [
+            body.title,
+            body.text,
+            body.platforms,
+            body.technologies,
+            body.githubUrl,
+            body.imageUrl,
+            body.imageOrientation,
+            body.placeInProjects,
+            request.params.id]
+    );
+    response.sendStatus(200);
+});
+
+router.delete('/:id', async (request, response) => {
+    if (!isAuthorized(request)) {
+        return response.status(401).json({ error: 'token missing or invalid' });
+    }
+    await client.query(
+        'DELETE FROM projects WHERE id = $1', [request.params.id]);
+    response.sendStatus(200);
 });
 
 module.exports = router;
