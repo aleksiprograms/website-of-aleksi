d055e50c2b644ae53f117f5de0e4a88a9a0bf63e
Aleksi Tolvanen <aleksiprograms@gmail.com>
2020-12-13 19:17:47 +0200

    Refactor project routes in backend

--- routes/api/projects.js
+++ routes/api/projects.js
@@ -46,109 +46,114 @@ const getTokenFrom = (request) => {
     return null;
 }
 
-router.get('/', async (request, response) => {
-    let result = await client.query('SELECT * FROM projects ORDER BY placeinprojects ASC');
-    let projects = [];
-    for (let row of result.rows) {
-        projects.push({
-            id: row.id,
-            title: row.title,
-            text: row.text,
-            platforms: row.platforms,
-            technologies: row.technologies,
-            githubUrl: row.githuburl,
-            imageUrl: row.imageurl,
-            imageOrientation: row.imageorientation,
-            placeInProjects: row.placeinprojects
-        });
-    }
-    response.status(200).json(projects);
+router.get('/', (request, response) => {
+    client.query('SELECT * FROM projects ORDER BY placeinprojects ASC')
+        .then((result) => {
+            let projects = [];
+            for (let row of result.rows) {
+                projects.push({
+                    id: row.id,
+                    title: row.title,
+                    text: row.text,
+                    platforms: row.platforms,
+                    technologies: row.technologies,
+                    githubUrl: row.githuburl,
+                    imageUrl: row.imageurl,
+                    imageOrientation: row.imageorientation,
+                    placeInProjects: row.placeinprojects
+                });
+            }
+            response.status(200).json(projects);
+        })
+        .catch((error) => {
+            response.sendStatus(400);
+        })
 });
 
-router.post('/', async (request, response) => {
+router.post('/', (request, response) => {
     if (!isAuthorized(request)) {
         return response.status(401).json({ error: 'token missing or invalid' });
     }
     const body = request.body;
-    try {
-        const result = await client.query(
-            'INSERT INTO projects '
-            + '('
-            + 'title, '
-            + 'text, '
-            + 'platforms, '
-            + 'technologies, '
-            + 'githuburl, '
-            + 'imageurl, '
-            + 'imageorientation, '
-            + 'placeinprojects'
-            + ') '
-            + 'VALUES($1, $2, $3, $4, $5, $6, $7, $8)'
-            + 'RETURNING id',
-            [
-                body.title,
-                body.text,
-                body.platforms,
-                body.technologies,
-                body.githubUrl,
-                body.imageUrl,
-                body.imageOrientation,
-                body.placeInProjects
-            ]
-        );
-        response.status(200).json({ id: result.rows[0].id });
-    } catch (error) {
-        response.sendStatus(400);
-    }
+    client.query(
+        'INSERT INTO projects '
+        + '('
+        + 'title, '
+        + 'text, '
+        + 'platforms, '
+        + 'technologies, '
+        + 'githuburl, '
+        + 'imageurl, '
+        + 'imageorientation, '
+        + 'placeinprojects'
+        + ') '
+        + 'VALUES($1, $2, $3, $4, $5, $6, $7, $8)'
+        + 'RETURNING id',
+        [
+            body.title,
+            body.text,
+            body.platforms,
+            body.technologies,
+            body.githubUrl,
+            body.imageUrl,
+            body.imageOrientation,
+            body.placeInProjects
+        ])
+        .then((result) => {
+            response.status(200).json({ id: result.rows[0].id });
+        })
+        .catch((error) => {
+            response.sendStatus(400);
+        })
 });
 
-router.put('/:id', async (request, response) => {
+router.put('/:id', (request, response) => {
     if (!isAuthorized(request)) {
         return response.status(401).json({ error: 'token missing or invalid' });
     }
     const body = request.body;
-    try {
-        await client.query(
-            'UPDATE projects '
-            + 'SET '
-            + 'title = $1, '
-            + 'text = $2, '
-            + 'platforms = $3, '
-            + 'technologies = $4, '
-            + 'githuburl = $5, '
-            + 'imageurl = $6, '
-            + 'imageorientation = $7, '
-            + 'placeinprojects = $8 '
-            + 'WHERE id = $9',
-            [
-                body.title,
-                body.text,
-                body.platforms,
-                body.technologies,
-                body.githubUrl,
-                body.imageUrl,
-                body.imageOrientation,
-                body.placeInProjects,
-                request.params.id
-            ]
-        );
-    } catch (error) {
-        response.sendStatus(400);
-    }
-    response.sendStatus(200);
+    client.query(
+        'UPDATE projects '
+        + 'SET '
+        + 'title = $1, '
+        + 'text = $2, '
+        + 'platforms = $3, '
+        + 'technologies = $4, '
+        + 'githuburl = $5, '
+        + 'imageurl = $6, '
+        + 'imageorientation = $7, '
+        + 'placeinprojects = $8 '
+        + 'WHERE id = $9',
+        [
+            body.title,
+            body.text,
+            body.platforms,
+            body.technologies,
+            body.githubUrl,
+            body.imageUrl,
+            body.imageOrientation,
+            body.placeInProjects,
+            request.params.id
+        ])
+        .then(() => {
+            response.sendStatus(200);
+        })
+        .catch((error) => {
+            response.sendStatus(400);
+        })
 });
 
-router.delete('/:id', async (request, response) => {
+router.delete('/:id', (request, response) => {
     if (!isAuthorized(request)) {
         return response.status(401).json({ error: 'token missing or invalid' });
     }
-    try {
-        await client.query(
-            'DELETE FROM projects WHERE id = $1', [request.params.id]);
-    } catch (error) {
-        response.sendStatus(400);
-    }
-    response.sendStatus(200);
+    client.query('DELETE FROM projects WHERE id = $1', [request.params.id])
+        .then(() => {
+            response.sendStatus(200);
+        })
+        .catch((error) => {
+            response.sendStatus(400);
+        })
 });
 
 module.exports = router;
