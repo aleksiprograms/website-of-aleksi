0574ee440ef3b8429801042c4bb955cae226a5eb
Aleksi Tolvanen <aleksiprograms@gmail.com>
2020-12-16 20:59:40 +0200

    Improve home view

--- client/src/views/HomeView.js
+++ client/src/views/HomeView.js
@@ -1,90 +1,66 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import styled from 'styled-components';
+import { useHistory } from 'react-router-dom';
+import {
+    Box,
+    Grid,
+    Typography,
+    Button,
+} from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 
-const Container = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    align-self: center;
-    justify-content: center;
-    margin-top: 1.5rem;
-`;
-
-const TextContainer = styled.div`
-    display: table;
-    margin-bottom: 1rem;
-`;
-
-const Title = styled.h1`
-    display: table-cell;
-    vertical-align: middle;
-    text-align: center;
-    font-size: 2rem;
-`;
-
-const Text = styled.p`
-    display: table-cell;
-    vertical-align: middle;
-    text-align: center;
-    font-size: 1.3rem;
-`;
-
-const Image = styled.img`
-    width: 80%;
-    margin-bottom: 1rem;
-    border-radius: 0.8rem;
-
-    @media (max-width: 1500px) {
-        width: 80%;
-    }
-    @media (max-width: 1200px) {
-        width: 90%;
-    }
-    @media (max-width: 900px) {
-        width: 95%;
-    }
-    @media (max-width: 600px) {
-        width: 100%;
-    }
-`;
-
-const Button = styled(Link)`
-    color: #fff;
-    background-color: #111;
-    text-decoration: none;
-    font-size: 1rem;
-    padding: 0.7rem 1.4rem;
-    border: 0.23rem solid #f00;
-    border-radius: 0.8rem;
-    margin-bottom: 1rem;
-
-    &:hover {
-        color: #000;
-        border: 0.23rem solid #000;
-        background-color: #f00;
-    }
-`;
+const useStyles = makeStyles((theme) => ({
+    image: {
+        borderRadius: 8,
+        [theme.breakpoints.up('xs')]: {
+            width: "100%",
+        },
+        [theme.breakpoints.up('sm')]: {
+            width: "85%",
+        },
+        [theme.breakpoints.up('md')]: {
+            width: "70%",
+        },
+    },
+}));
 
 const HomeView = () => {
+
+    const classes = useStyles();
+    const history = useHistory();
+
     return (
-        <Container>
-            <TextContainer>
-                <Title>Hey, I am Aleksi</Title>
-            </TextContainer>
-            <TextContainer>
-                <Text>
-                    I am mainly self-taught programmer/developer and
-                    I am interested in mobiledev, webdev and gamedev.
-                </Text>
-            </TextContainer>
-            <Button to="/projects">PROJECTS</Button>
-            <Image
-                src="https://dl.dropboxusercontent.com/s/q2mf0ugw4ohzrlm/battleagainstshapesart.png?dl=0"
-                alt="Home page art"
-            />
-        </Container>
+        <Box mt={2} mb={2}>
+            <Grid container spacing={2}>
+                <Grid item container justify="center">
+                    <Typography variant="h4">
+                        Hey, I am Aleksi
+                    </Typography>
+                </Grid>
+                <Grid item container justify="center">
+                    <Typography variant="h6" align="center">
+                        I am mainly self-taught programmer/developer and
+                        I am interested in mobiledev, webdev and gamedev.
+                    </Typography>
+                </Grid>
+                <Grid item container justify="center">
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        onClick={() => history.push("/projects")}
+                    >
+                        Projects
+                    </Button>
+                </Grid>
+                <Grid item container justify="center">
+                    <img
+                        src="https://dl.dropboxusercontent.com/s/q2mf0ugw4ohzrlm/battleagainstshapesart.png?dl=0"
+                        alt="Home page art"
+                        className={classes.image}
+                    />
+                </Grid>
+            </Grid>
+        </Box>
     );
-};
+}
 
 export default HomeView;
