2c994dfbe21eb7f8383076b831b699471d8a2c9d
Aleksi Tolvanen <aleksiprograms@gmail.com>
2020-12-17 18:38:59 +0200

    Improve login view

--- client/src/views/LoginView.js
+++ client/src/views/LoginView.js
@@ -1,93 +1,43 @@
 import React, { useState, useEffect, useContext } from 'react';
 import { Redirect } from "react-router-dom";
-import styled from 'styled-components';
+import {
+    Box,
+    Grid,
+    Typography,
+    Button,
+    TextField,
+} from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 import { UserContext } from '../context/UserContext';
 import useUserApi from '../hooks/useUserApi';
 
-const Container = styled.div`
-    display: flex;
-    flex-direction: column;
-    margin-top: 6rem;
-    margin-bottom: 1rem;
-    margin-left: auto;
-    margin-right: auto;
-    width: 50%;
-    
-    @media (max-width: 900px) {
-        width: 60%;
-    }
-    @media (max-width: 600px) {
-        width: 70%;
-    }
-`;
-
-const Form = styled.form`
-    display: flex;
-    flex-direction: column;
-`;
-
-const Title = styled.h2`
-    color: #000;
-    font-size: 1.5rem;
-    align-self: center;
-    margin-bottom: 0.3rem;
-`;
-
-const ErrorMessage = styled.p`
-    color: #f00;
-    font-size: 1.3rem;
-    height: 1.3rem;
-    align-items: flex-end;
-    align-self: center;
-`;
-
-const FormRow = styled.div`
-    display: flex;
-    flex: 1 0 auto;
-    flex-direction: column;
-    margin-top: 0.3rem;
-`;
-
-const InputTitle = styled.p`
-    color: #000;
-    font-size: 1rem;
-`;
-
-const Input = styled.input`
-    color: #000;
-    font-size: 0.85rem;
-    width: 100%;
-`;
-
-const Button = styled.button`
-    color: #fff;
-    background-color: #111;
-    text-decoration: none;
-    font-size: 1rem;
-    border: 0.20rem solid #00f;
-    border-radius: 0.5rem;
-    width: 5.6rem;
-    height: 2.3rem;
-    margin-top: 1rem;
-    align-self: center;
-
-    &:hover {
-        background-color: #00f;
-    }
-`;
+const useStyles = makeStyles((theme) => ({
+    field: {
+        [theme.breakpoints.up('xs')]: {
+            width: "60%",
+        },
+        [theme.breakpoints.up('sm')]: {
+            width: "50%",
+        },
+        [theme.breakpoints.up('md')]: {
+            width: "40%",
+        },
+    },
+}));
 
 const LoginView = () => {
 
+    const classes = useStyles();
     const userContext = useContext(UserContext);
     const userApi = useUserApi();
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
-    const [errorMessage, setErrorMessage] = useState('');
+    const [error, setError] = useState('');
 
     useEffect(() => {
         setUsername('');
         setPassword('');
-        setErrorMessage(userContext.error);
+        setError(userContext.error);
     }, [userContext.error]);
 
     if (userContext.user != null) {
@@ -96,36 +46,60 @@ const LoginView = () => {
         );
     }
 
-    const login = (event) => {
-        event.preventDefault();
+    const login = () => {
+        setError("");
         userApi.login(username, password);
     }
 
     return (
-        <Container>
-            <Title>ADMIN LOGIN</Title>
-            <ErrorMessage>{errorMessage}</ErrorMessage>
-            <Form onSubmit={login}>
-                <FormRow>
-                    <InputTitle>Username</InputTitle>
-                    <Input
-                        type="text"
+        <Box mt={10} mb={2}>
+            <Grid container spacing={2}>
+                <Grid item container justify="center">
+                    <Typography variant="h5">
+                        Admin login
+                    </Typography>
+                </Grid>
+                {error &&
+                    <Grid item container justify="center">
+                        <Typography style={{ color: "#f00" }}>
+                            {error}
+                        </Typography>
+                    </Grid>
+                }
+                <Grid item container justify="center">
+                    <TextField
+                        label="Username"
                         value={username}
-                        onChange={({ target }) => setUsername(target.value)}
+                        onChange={(event) => setUsername(event.target.value)}
+                        fullWidth
+                        variant="outlined"
+                        autoFocus
+                        className={classes.field}
                     />
-                </FormRow>
-                <FormRow>
-                    <InputTitle>Password</InputTitle>
-                    <Input
-                        type="password"
+                </Grid>
+                <Grid item container justify="center">
+                    <TextField
+                        label="Password"
                         value={password}
-                        onChange={({ target }) => setPassword(target.value)}
+                        onChange={(event) => setPassword(event.target.value)}
+                        fullWidth
+                        variant="outlined"
+                        type="password"
+                        className={classes.field}
                     />
-                </FormRow>
-                <Button type="submit">LOG IN</Button>
-            </Form>
-        </Container>
+                </Grid>
+                <Grid item container justify="center">
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        onClick={login}
+                    >
+                        Login
+                    </Button>
+                </Grid>
+            </Grid>
+        </Box>
     );
-};
+}
 
 export default LoginView;
