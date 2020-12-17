c3517b20f177dba341515102a4b96cc665075244
Aleksi Tolvanen <aleksiprograms@gmail.com>
2020-12-17 20:31:59 +0200

    Improve admin view

--- client/src/views/AdminView.js
+++ client/src/views/AdminView.js
@@ -1,38 +1,23 @@
 import React, { useContext } from 'react';
 import { Redirect, useHistory } from "react-router-dom";
-import styled from 'styled-components';
+import {
+    Box,
+    Grid,
+    Typography,
+    Button,
+    List,
+    ListItem,
+    ListItemText,
+    ListItemSecondaryAction,
+    IconButton,
+    Tooltip,
+} from '@material-ui/core';
+import EditIcon from '@material-ui/icons/Edit';
+import DeleteIcon from '@material-ui/icons/Delete';
 import { UserContext } from '../context/UserContext';
 import { ProjectContext } from '../context/ProjectContext';
 import useUserApi from '../hooks/useUserApi';
-import ProjectAdmin from '../components/ProjectAdmin';
-
-const TitleContainer = styled.div`
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: space-between;
-    margin-top: 1rem;
-`;
-
-const Title = styled.h2`
-    color: #000;
-    font-size: 2rem;
-`;
-
-const Button = styled.button`
-    color: #fff;
-    background-color: #111;
-    text-decoration: none;
-    font-size: 1rem;
-    border: 0.20rem solid #f00;
-    border-radius: 0.5rem;
-    width: 7rem;
-    height: 2.3rem;
-
-    &:hover {
-        background-color: #f00;
-    }
-`;
+import useProjectApi from '../hooks/useProjectApi';
 
 const AdminView = () => {
 
@@ -40,6 +25,7 @@ const AdminView = () => {
     const userContext = useContext(UserContext);
     const projectContext = useContext(ProjectContext);
     const userApi = useUserApi();
+    const projectApi = useProjectApi();
 
     if (userContext.user == null) {
         return (
@@ -55,27 +41,79 @@ const AdminView = () => {
         history.push('/create');
     }
 
+    const edit = (project) => {
+        history.push({
+            pathname: '/create',
+            state: { project }
+        });
+    }
+
+    const remove = (id) => {
+        projectApi.removeProject(id);
+    }
+
     return (
-        <div>
-            <TitleContainer>
-                <Title>Admin Dashboard</Title>
-                <Button onClick={logout}>LOG OUT</Button>
-            </TitleContainer>
-            <div>
-                <TitleContainer>
-                    <Title>Projects</Title>
-                    <Button onClick={add}>ADD PROJECT</Button>
-                </TitleContainer>
-                {projectContext.projects.map((project) => {
-                    return (
-                        <ProjectAdmin
-                            key={project.id}
-                            project={project}
-                        />
-                    );
-                })}
-            </div>
-        </div>
+        <Box mt={2} mb={2}>
+            <Grid container spacing={2}>
+                <Grid item container justify="center">
+                    <Grid container justify="space-between">
+                        <Typography variant="h5">
+                            Admin Dashboard
+                        </Typography>
+                        <Button
+                            variant="contained"
+                            color="primary"
+                            onClick={logout}
+                        >
+                            Logout
+                        </Button>
+                    </Grid>
+                </Grid>
+                <Grid item container justify="center">
+                    <Grid container justify="space-between">
+                        <Typography variant="h6">
+                            Projects
+                        </Typography>
+                        <Button
+                            color="primary"
+                            onClick={add}
+                        >
+                            Add project
+                        </Button>
+                    </Grid>
+                    <List style={{ width: "100%" }} disablePadding>
+                        {projectContext.projects.map((project) => {
+                            return (
+                                <ListItem key={project.id} disableGutters>
+                                    <ListItemText
+                                        primary={project.title}
+                                    />
+                                    <ListItemSecondaryAction>
+                                        <>
+                                            <Tooltip title="Edit">
+                                                <IconButton
+                                                    onClick={() => edit(project)}
+                                                >
+                                                    <EditIcon />
+                                                </IconButton>
+                                            </Tooltip>
+                                            <Tooltip title="Delete">
+                                                <IconButton
+                                                    edge="end"
+                                                    onClick={() => remove(project.id)}
+                                                >
+                                                    <DeleteIcon />
+                                                </IconButton>
+                                            </Tooltip>
+                                        </>
+                                    </ListItemSecondaryAction>
+                                </ListItem>
+                            );
+                        })}
+                    </List>
+                </Grid>
+            </Grid>
+        </Box>
     );
 }
 
